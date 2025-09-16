5e67e98eb6e8eacfd2adfd74343fcc8b9fcfa73c
RedEye-Developers <reswinroyal@gmail.com>
2025-09-17 00:52:35 +0530

    modified

--- vars/helmDeploy.groovy
+++ vars/helmDeploy.groovy
@@ -1,4 +1,10 @@
 def call(String helmFolderPath, String serviceName)
 {
-    sh 'helm upgrade -i ./${helmFolderPath}/values.yaml ${serviceName} ./$helmFolderPath --wait'
+    try {
+        sh 'helm upgrade -i ./${helmFolderPath}/values.yaml ${serviceName} ./$helmFolderPath --wait'
+        echo "K8s Deployment Successfully Completed!"
+    } catch (err) {
+        echo "K8s Deployment Failed!"
+        throw err
+    }
 }
