ad54d90ee0519ffd4c7cbc2f06634b8fb788d4be
RedEye-Developers <reswinroyal@gmail.com>
2025-09-16 23:53:19 +0530

    modified

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,8 +1,4 @@
 def call()
 {
-    sh """
-    echo GitCread : ${evn.GITHUB_CRED}
-    echo GitProject : ${evn.GITHUB_PROJECT_REPO}
-    """
-    git credentialsId: env.GITHUB_CRED, url: evn.GITHUB_PROJECT_REPO
+    sh "echo ${evn.GITHUB_CRED}"
 }
