d35b597ffcad58ef3df21e1a0b42d7908db12167
RedEye-Developers <reswinroyal@gmail.com>
2025-09-16 23:00:13 +0530

    modified

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,8 +2,7 @@ def call(String dockerFilePath, String dockerBuildName, String dockerBuildTag)
 {
     try {
         sh "docker build -f ./${dockerFilePath}/Dockerfile -t ${dockerBuildName}:${dockerBuildTag} ."
-        echo "Docker Build was Success!"
-    } catch (ExceptionName) {
-        echo "Docker Build Failed!"
+    } catch (err) {
+        throw err
     }
 }
